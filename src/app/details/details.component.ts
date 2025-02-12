b8c85dc42a1bae94ed668b7ee2b69ba678a5e977
rcaa <rodrigo.andrade@ufape.edu.br>
2025-02-12 08:25:00 -0300

    atualizando componente de details e o service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -57,7 +57,8 @@ export class DetailsComponent {
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {
+    this.housingService.getHousingLocationById(housingLocationId)
+     .then((housingLocation) => {
       this.housingLocation = housingLocation;})
   };
 
